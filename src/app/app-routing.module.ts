909f86dadd97272504512f725bdf5b800d382493
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2023-11-09 21:08:46 -0300

    Att

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NoticiasComponent } from './Site/noticias/noticias.component';
+import { InicioComponent } from './Site/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'noticias', component:NoticiasComponent},
+  {path: 'inicio', component:InicioComponent},
+  {path:'',
+  redirectTo: '/inicio',
+  pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
