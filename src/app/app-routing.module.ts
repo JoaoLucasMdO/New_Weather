65b7c0a739ca66e0f595fb4239ebd931311c5c76
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2023-11-15 22:16:22 -0300

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NoticiasComponent } from './Site/noticias/noticias.component';
 import { InicioComponent } from './Site/inicio/inicio.component';
+import { ContatosComponent } from './Site/contatos/contatos.component';
+import { PlanosComponent } from './Site/planos/planos.component';
+import { RedesEstacoesComponent } from './Site/redes-estacoes/redes-estacoes.component';
+import { CadastroComponent } from './Cliente/cadastro/cadastro.component';
+import { LoginComponent } from './Cliente/login/login.component';
 
 const routes: Routes = [
-  {path: 'noticias', component:NoticiasComponent},
+  {path: 'cadastro', component:CadastroComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'contatos', component:ContatosComponent},
   {path: 'inicio', component:InicioComponent},
+  {path: 'noticias', component:NoticiasComponent},
+  {path: 'planos', component:PlanosComponent},
+  {path: 'redes-estacoes', component:RedesEstacoesComponent},
   {path:'',
   redirectTo: '/inicio',
   pathMatch:'full'}
