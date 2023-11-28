d4d0e5187edd26bdefbd063ebb0bbc18fb32a1a5
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2023-11-27 22:33:40 -0300

    att

--- src/app/Cliente/login/login.component.ts
+++ src/app/Cliente/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { ClienteService } from '../shared/cliente.service';
 import { Cliente } from '../shared/cliente';
 
@@ -7,10 +8,23 @@ import { Cliente } from '../shared/cliente';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent{
-  private logado : boolean = false;
-  private 'clienteService' : ClienteService;
-  
-  
+export class LoginComponent {
+  private logado: boolean = false;
 
+  constructor(private router: Router) {}
+
+ testarLogin() {
+    let login = (<HTMLInputElement>document.getElementById('email'))?.value;
+    let senha = (<HTMLInputElement>document.getElementById('senha'))?.value;
+
+    if ((login === "gesley.rosa@fatec.sp.gov.br" && senha === '12345') || (login === "joao.lucas@fatec.sp.gov.br" && senha === '12345')) {
+      this.router.navigate(['/admHome']);
+    } else () => {
+      // Tratamento para credenciais incorretas, se necessário
+      console.log("Credenciais incorretas");
+    }
+  }
 }
+
+
+
