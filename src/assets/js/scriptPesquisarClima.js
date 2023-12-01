11a99fbff048171cf16514a4e8442cfc63c215b9
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2023-11-30 21:21:01 -0300

    att

--- src/assets/js/scriptPesquisarClima.js
+++ src/assets/js/scriptPesquisarClima.js
@@ -4,9 +4,8 @@ climaIconElement = document.querySelector("#clima-icon");
 dadosTemp = document.querySelector(".dadosTemperatura")
 dadosLog = document.querySelector(".dadosLogradouro")
 
-document.querySelector("#btnVerificarClima").addEventListener("click", async (e) => {
-    e.preventDefault();
-    let cidade = document.querySelector("input[name='pesquisarCidade']").value
+async function oi() {
+  let cidade = document.querySelector("input[name='s']").value;
     const apiClimaURL = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${apiKey}&lang=pt_br`
     const res = await fetch(apiClimaURL)
     const data = await res.json()
@@ -21,13 +20,27 @@ document.querySelector("#btnVerificarClima").addEventListener("click", async (e)
     let temperaturaMax = parseFloat(data.main.temp_max);
     let umidade = parseFloat(data.main.humidity);
     let vento = parseFloat(data.wind.speed);
-    let tempo = data.weather[0].description;
+    //let tempo = data.weather[0].description;
     let pressao = data.main.pressure;
+    let sensacaoTermica = data.main.feels_like;
+    // Funções para nascer e descer do sol
+    let timestampSunrise = data.main.sunrise;
+    let timestampSunset = data.main.sunset;
+    // Converter timestamps para objetos Date
+    let dataSunrise = new Date(timestampSunrise * 1000);
+    let dataSunset = new Date(timestampSunset * 1000);
+    // Extrair horas, minutos e segundos
+    let horaSunrise = dataSunrise.toISOString().substr(11, 8);
+    let horaSunset = dataSunset.toISOString().substr(11, 8);
+
     document.querySelector('#temperatura').innerHTML = `Temperatura: ${temperatura} º`
     document.querySelector('#temperaturaMinMax').innerHTML = `Max: ${temperaturaMax} º / Mín: ${temperaturaMin} º`
     document.querySelector('#umidade').innerHTML = `Umidade: ${umidade} %`
     document.querySelector('#pressao').innerHTML = `Pressão: ${pressao} mb`
     document.querySelector('#vento').innerHTML = `Vento: ${vento} km/h`
     document.querySelector('#tempo').innerHTML = `Tempo: ${tempo}`;
-});
+    document.querySelector('#sensacaoTermica').innerHTML = `Tempo: ${sensacaoTermica}`;
+    document.querySelector('#sunrise').innerHTML = `Nascer do sol: ${horaSunrise}`;
+    document.querySelector('#sunset').innerHTML = `Descer do sol: ${horaSunset}`;
+};
 
